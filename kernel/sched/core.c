8595253a805fe9a8f22a3dcfd61cd15f17ea6eee
Michael S. Tsirkin <mst@redhat.com>
2013-09-22 17:20:54 +0300

    sched: Micro-optimize by dropping unnecessary task_rq() calls
    We always know the rq used, let's just pass it around.
    This seems to cut the size of scheduler core down a tiny bit:
    
    Before:
    
      [linux]$ size kernel/sched/core.o.orig
         text    data     bss     dec     hex filename
        62760   16130    3876   82766   1434e kernel/sched/core.o.orig
    
    After:
    
      [linux]$ size kernel/sched/core.o.patched
         text    data     bss     dec     hex filename
        62566   16130    3876   82572   1428c kernel/sched/core.o.patched
    
    Probably speeds it up as well.
    
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>
    Signed-off-by: Peter Zijlstra <peterz@infradead.org>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -825,7 +825,7 @@ static void set_load_weight(struct task_struct *p)
 static void enqueue_task(struct rq *rq, struct task_struct *p, int flags)
 {
 	update_rq_clock(rq);
-	sched_info_queued(p);
+	sched_info_queued(rq, p);
 	p->sched_class->enqueue_task(rq, p, flags);
 	trace_sched_enq_deq_task(p, 1);
 	inc_cumulative_runnable_avg(rq, p);
@@ -834,7 +834,7 @@ static void enqueue_task(struct rq *rq, struct task_struct *p, int flags)
 static void dequeue_task(struct rq *rq, struct task_struct *p, int flags)
 {
 	update_rq_clock(rq);
-	sched_info_dequeued(p);
+	sched_info_dequeued(rq, p);
 	p->sched_class->dequeue_task(rq, p, flags);
 	trace_sched_enq_deq_task(p, 0);
 	dec_cumulative_runnable_avg(rq, p);
@@ -2098,7 +2098,7 @@ prepare_task_switch(struct rq *rq, struct task_struct *prev,
 		    struct task_struct *next)
 {
 	trace_sched_switch(prev, next);
-	sched_info_switch(prev, next);
+	sched_info_switch(rq, prev, next);
 	perf_event_task_sched_out(prev, next);
 	fire_sched_out_preempt_notifiers(prev, next);
 	prepare_lock_switch(rq, next);
