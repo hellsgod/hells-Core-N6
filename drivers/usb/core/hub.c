33a4b8c8c31a4de765b7a96893d0a12ac94918f9
flar2 <asegaert@gmail.com>
2014-12-01 20:27:00 -0500

    fix compilation warnings
    Signed-off-by: engstk <eng.stk@sapo.pt>

--- drivers/usb/core/hub.c
+++ drivers/usb/core/hub.c
@@ -3298,7 +3298,7 @@ int usb_port_resume(struct usb_device *udev, pm_message_t msg)
 	struct usb_port *port_dev;
 	int		port1 = udev->portnum;
 	int		status;
-	u16		portchange, portstatus;
+	u16		portchange = 0, portstatus = 0;
 
 	if (!hub)
 		return -ENODEV;
@@ -3980,7 +3980,7 @@ int hub_port_debounce(struct usb_hub *hub, int port1, bool must_be_connected)
 {
 	int ret;
 	int total_time, stable_time = 0;
-	u16 portchange, portstatus;
+	u16 portchange = 0, portstatus = 0;
 	unsigned connection = 0xffff;
 
 	for (total_time = 0; ; total_time += HUB_DEBOUNCE_STEP) {
@@ -4746,10 +4746,10 @@ static void hub_events(void)
 	struct usb_interface *intf;
 	struct usb_hub *hub;
 	struct device *hub_dev;
-	u16 hubstatus;
-	u16 hubchange;
-	u16 portstatus;
-	u16 portchange;
+	u16 hubstatus = 0;
+	u16 hubchange = 0;
+	u16 portstatus = 0;
+	u16 portchange = 0;
 	int i, ret;
 	int connect_change, wakeup_change;
 
